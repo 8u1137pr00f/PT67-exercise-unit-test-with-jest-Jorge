0239b2d2c0021bf0eff0cb638d84d76f441e169c
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-05-21 18:27:03 +0000

    2da entrega, eliminadas variables CONST innecesarias.

--- test.js
+++ test.js
@@ -7,19 +7,13 @@ test('adds 14 + 9 to equal 23', () => {
 })
 
 test("One euro should be 1.07 dollars", function() {
-    const dollars = fromEuroToDollar(3.5);
-    const expected = 3.5 * 1.07;
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
 
 test ("One dollar should be 146.17 yens", function () {
-    const dollars = fromDollarToYen(3.5);
-    const expected = 3.5 * 146.16;
-    expect(fromDollarToYen(3.5)).toBe(511.595); // 1 dollar is 146.17 yens, then 3.5 dollars should be = (3.5 * 146.17)
+    expect(fromDollarToYen(3.5)).toBe(511.595); // 1 dollar is 146.16 yens, then 3.5 dollars should be = (3.5 * 146.16)
 })
 
 test ("1000 yens should be 5.556 pounds", function () {
-    const yens = fromYenToPound(1000);
-    const expected = 1000 / 179.98;
     expect(fromYenToPound(1000)).toBe(5.556); //1 yen equals 0.00555 pounds, then 1000 yens should be = (1000 * 0.00555 = 1000 / 179.98)
 })
